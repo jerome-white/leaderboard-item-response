f3fc28d97bc6628fa730bf549ee25aae5da04028
Jerome White <jerome@thewex.net>
2024-03-08 11:07:00 +0000

    Corrected CSV writing

--- hf-aggregator.py
+++ hf-aggregator.py
@@ -200,4 +200,4 @@ if __name__ == '__main__':
                 if writer is None:
                     writer = csv.DictWriter(sys.stdout, fieldnames=rows[0])
                     writer.writeheader()
-                writer.writerow(rows)
+                writer.writerows(rows)
