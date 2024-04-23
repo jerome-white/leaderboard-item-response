5d1a9a86fb88dc63074076bd5b5fb8e7629afddf
Jerome White <jerome@thewex.net>
2024-04-23 07:52:24 +0000

    Handle loggin verbosity in the module

--- logutils.py
+++ logutils.py
@@ -1,11 +1,21 @@
 import os
 import logging
 
+from datasets import logging as dl
+
+#
+#
+#
 logging.basicConfig(
     format='[ %(asctime)s %(levelname)s %(filename)s ] %(message)s',
     datefmt='%H:%M:%S',
     level=os.environ.get('PYTHONLOGLEVEL', 'WARNING').upper(),
 )
 logging.captureWarnings(True)
-# logging.getLogger('matplotlib').setLevel(logging.CRITICAL)
 Logger = logging.getLogger(__name__)
+
+#
+#
+#
+dl.disable_progress_bar()
+dl.set_verbosity(dl.CRITICAL)
