6bfa2879eaea0709c20b1111f6a52cf9248e581b
Jerome White <jerome@thewex.net>
2024-04-29 07:04:28 +0000

    Storage parallelized across processes

--- mylib/output_utils.py
+++ mylib/output_utils.py
@@ -1,13 +1,15 @@
 import csv
-from io import TextIOWrapper
-from urllib.parse import ParseResult, urlunparse
+from pathlib import Path
+from tempfile import NamedTemporaryFile
+from urllib.parse import urlunparse
 
 import pandas as pd
 import awswrangler as wr
 
 class ChunkedDataWriter:
-    def __init__(self, chunk_size: int):
+    def __init__(self, destination, chunk_size):
         self.chunk_size = chunk_size
+        self.destination = destination
         self.cache = []
 
     def __enter__(self):
@@ -28,38 +30,41 @@ class ChunkedDataWriter:
     def flush(self):
         raise NotImplementedError()
 
-class CSVWriter(ChunkedDataWriter):
-    def	__init__(self, chunk_size: int, fp: TextIOWrapper):
-        super().__init__(chunk_size)
-
-        self.fp = fp
-        self.writer = None
+class CSVFileWriter(ChunkedDataWriter):
+    def __init__(self, destination, chunk_size):
+        super().__init__(Path(destination.path), chunk_size)
 
     def flush(self):
-        if self.writer is None:
-            fieldnames = self.cache[0][0]
-            self.writer = csv.DictWriter(self.fp, fieldnames=fieldnames)
-            self.writer.writeheader()
-        self.writer.writerows(self.cache)
+        fieldnames = self.cache[0][0]
+        with NamedTemporaryFile(mode='w',
+                                suffix='.csv',
+                                prefix='',
+                                dir=self.destination,
+                                delete=False) as fp:
+            writer = csv.DictWriter(fp, fieldnames=fieldnames)
+            writer.writeheader()
+            writer.writerows(self.cache)
 
 class SimpleStorageWriter(ChunkedDataWriter):
-    def __init__(self, chunk_size: int, bucket: ParseResult):
-        super().__init__(chunk_size)
-        self.path = urlunparse(bucket)
+    def __init__(self, destination, chunk_size):
+        super().__init__(urlunparse(destination), chunk_size)
 
     def flush(self):
-        df = pd.DataFrame.from_records(self.cache)
+        df = (pd
+              .DataFrame
+              .from_records(self.cache)
+              .astype({
+                  'date': 'timedelta64[ns]',
+                  'value': float,
+              }))
         wr.s3.to_parquet(
             df=df,
-            path=self.path,
+            path=self.destination,
             dataset=True,
             use_threads=True,
             compression='gzip',
             partition_cols=[
                 'task',
-                'author',
+                # 'author',
             ],
-            dtype={
-                'date': 'date',
-            },
         )
--- store-evals.py
+++ store-evals.py
@@ -1,26 +1,52 @@
 import sys
 import csv
-import functools as ft
-from urllib.parse import urlparse
+import random
 from argparse import ArgumentParser
+from urllib.parse import urlparse
+from multiprocessing import Pool, JoinableQueue
 
-from mylib import Logger, CSVWriter, SimpleStorageWriter
+from mylib import Logger, CSVFileWriter, SimpleStorageWriter
+
+def func(queue, args):
+    Writer = SimpleStorageWriter if args.output.scheme else CSVFileWriter
+    with Writer(args.output, args.chunk_size) as writer:
+        while True:
+            rows = queue.get()
+            flushed = writer.write(rows)
+            if flushed:
+                Logger.info(f'Flushed {args.chunk_size}')
+            queue.task_done()
+
+def gather(fp):
+    records = []
+    assert args.chunk_size > 1
+    limit = random.randint(1, args.chunk_size)
+
+    reader = csv.DictReader(fp)
+    for row in reader:
+        records.append(row)
+        if records > limit:
+            yield records
+            records = []
+            limit = random.randint(1, args.chunk_size)
+
+    if records:
+        yield records
 
 if __name__ == '__main__':
     arguments = ArgumentParser()
-    arguments.add_argument('--bucket', type=urlparse)
-    arguments.add_argument('--chunk-size', type=int, default=int(1e4))
+    arguments.add_argument('--output', type=urlparse)
+    arguments.add_argument('--chunk-size', type=int, default=int(1e5))
     arguments.add_argument('--workers', type=int)
     args = arguments.parse_args()
 
-    reader = csv.DictReader(sys.stdin)
-    if args.bucket:
-        Writer = ft.partial(SimpleStorageWriter, bucket=args.bucket)
-    else:
-        Writer = ft.partial(CSVWriter, fp=sys.stdout)
+    queue = JoinableQueue()
+    initargs = (
+        queue,
+        args,
+    )
 
-    with Writer(chunk_size=args.chunk_size) as writer:
-        for row in reader:
-            flushed = writer.write(row)
-            if flushed:
-                Logger.info(f'Flushed {args.chunk_size}')
+    with Pool(args.workers, func, initargs):
+        for rows in gather(sys.stdin):
+            queue.put(rows)
+        queue.join()
