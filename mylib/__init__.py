06d4852f0bc4b930aa5bd25248cccff81c99aba6
Jerome White <jerome@thewex.net>
2024-05-02 19:35:06 +0530

    Add AuthorModel to export list

--- mylib/__init__.py
+++ mylib/__init__.py
@@ -1,3 +1,8 @@
-from .my_types import EvaluationSet, EvaluationInfo, LeaderboardResult
+from .my_types import (
+    AuthorModel,
+    EvaluationSet,
+    EvaluationInfo,
+    LeaderboardResult,
+)
 from .log_utils import Logger
 from .output_utils import CSVFileWriter, SimpleStorageWriter
