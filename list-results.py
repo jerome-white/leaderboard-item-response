9626d35fffabba1dd8b967505857e2f5064f9eed
Jerome White <jerome@thewex.net>
2024-05-07 07:13:55 +0000

    Gracefully handle HTTP errors by delaying subsequent requests

--- list-results.py
+++ list-results.py
@@ -1,4 +1,8 @@
+import os
 import sys
+import time
+import random
+import itertools as it
 from pathlib import Path
 from datetime import datetime
 from argparse import ArgumentParser
@@ -6,6 +10,7 @@ from dataclasses import dataclass
 from multiprocessing import Pool, Queue
 
 from huggingface_hub import HfFileSystem
+from huggingface_hub.utils import HfHubHTTPError
 
 from mylib import Logger, DatasetPathHandler, hf_datetime
 
@@ -17,12 +22,45 @@ class Result:
     def __lt__(self, other):
         return self.dt < other.dt
 
+class Backoff:
+    _backoff_factor = 2
+
+    def __init__(self, backoff, fuzz=0):
+        self.backoff = backoff
+        self.fuzz = fuzz
+
+    def __iter__(self):
+        backoff = self.backoff
+        while True:
+            if self.fuzz:
+                backoff += backoff * random.uniform(-self.fuzz, self.fuzz)
+            yield backoff
+
+            backoff *= self._backoff_factor
+
 class DatasetFileSystem:
-    def __init__(self):
-        self.fs = HfFileSystem()
+    def __init__(self, token, backoff):
+        self.fs = HfFileSystem(token=token)
+        self.backoff = backoff
+
+    def walk(self, target):
+        for (i, j) in enumerate(self.backoff, 1):
+            try:
+                yield from self.fs.ls(target)
+                break
+            except HfHubHTTPError as err:
+                Logger.error(
+                    '%s: request=%s message=%s (attempt=%d, backoff=%ds)',
+                    err,
+                    err.request_id,
+                    err.server_message,
+                    i,
+                    j,
+                )
+            time.sleep(j)
 
     def options(self, target):
-        for i in self.fs.ls(target):
+        for i in self.walk(target):
             if i['type'] == 'directory':
                 path = Path(i['name'])
                 try:
@@ -32,11 +70,12 @@ class DatasetFileSystem:
                 yield Result(path, dt)
 
     def results(self, target):
-        for i in self.fs.ls(target):
+        for i in self.walk(target):
             yield i['name']
 
-def func(incoming, outgoing):
-    fs = DatasetFileSystem()
+def func(incoming, outgoing, args):
+    backoff = Backoff(args.backoff, 0.1)
+    fs = DatasetFileSystem(os.getenv('HF_BEARER_TOKEN'), backoff)
     target = DatasetPathHandler()
 
     while True:
@@ -53,6 +92,7 @@ def func(incoming, outgoing):
 
 if __name__ == '__main__':
     arguments = ArgumentParser()
+    arguments.add_argument('--backoff', type=float, default=15)
     arguments.add_argument('--workers', type=int)
     args = arguments.parse_args()
 
@@ -61,6 +101,7 @@ if __name__ == '__main__':
     initargs = (
         outgoing,
         incoming,
+        args,
     )
 
     with Pool(args.workers, func, initargs):
