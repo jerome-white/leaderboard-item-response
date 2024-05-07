175cc3bc1c095aef43e774247070063a8f1d6eb1
Jerome White <jerome@thewex.net>
2024-05-07 04:25:41 +0000

    Tools to manage HuggingFace URLs

--- mylib/__init__.py
+++ mylib/__init__.py
@@ -1,10 +1,4 @@
-from .my_types import (
-    AuthorModel,
-    EvaluationSet,
-    EvaluationInfo,
-    LeaderboardValue,
-    LeaderboardResult,
-)
 from .log_utils import Logger
+from .url_utils import DatasetPathHandler
 from .date_utils import hf_datetime
 from .output_utils import CSVFileWriter, SimpleStorageWriter
