837592afc9b2c2538c69f580121c58fbc9f708c0
Jerome White <jerome@thewex.net>
2024-05-08 13:24:53 +0000

    Ensure the checksum exists before validating

--- scripts/retrieve/extract-evals.py
+++ scripts/retrieve/extract-evals.py
@@ -50,23 +50,25 @@ class FloatAdapter:
 class FileChecksum:
     _method = 'md5'
 
-    def __init__(self, path):
-        self.path = path
-        while path.suffix:
-            path = path.with_suffix('')
-        self.target = path.with_suffix(f'.{self._method}')
+    def __init__(self, data):
+        self.data = data
+        while data.suffix:
+            data = data.with_suffix('')
+        self.checksum = data.with_suffix(f'.{self._method}')
 
     def __str__(self):
-        with self.path.open('rb') as fp:
+        with self.data.open('rb') as fp:
             digest = hashlib.file_digest(fp, self._method)
 
         return digest.hexdigest()
 
     def __bool__(self):
-        return str(self) == self.target.read_text().strip()
+        if not self.checksum.exists():
+            return False
+        return str(self) == self.checksum.read_text().strip()
 
     def write(self):
-        with self.target.open('w') as fp:
+        with self.checksum.open('w') as fp:
             print(self, file=fp)
 
 @dataclass
