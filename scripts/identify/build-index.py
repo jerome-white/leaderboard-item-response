7af142622efa0c0591d0bf0b60606feb866cda12
Jerome White <jerome@thewex.net>
2024-05-13 14:22:43 +0000

    Change directory layout
    Info files contain multiple dataset results. They are grouped-by their
    author and model. The previous method of individual files became
    unwieldy.

--- scripts/identify/build-index.py
+++ scripts/identify/build-index.py
@@ -1,25 +1,45 @@
 import sys
+import collections as cl
 from pathlib import Path
 from argparse import ArgumentParser
+from dataclasses import dataclass
 
-from mylib import Logger, hash_it
+from mylib import Logger, AuthorModel, hash_it
+
+@dataclass(frozen=True)
+class DataGroup:
+    target: Path
+
+    def __repr__(self):
+        return hash_it(str(self.target.parent), 16)
+
+def group(fp):
+    db = cl.defaultdict(set)
+    hf_root = Path('datasets', 'open-llm-leaderboard')
+
+    for i in fp:
+        path = Path(i.strip())
+
+        (details, *_) = path.relative_to(hf_root).parts
+        am = AuthorModel(details)
+        key = DataGroup(Path(am.author, am.model))
+
+        db[key].add(path)
+
+    yield from db.items()
 
 if __name__ == '__main__':
     arguments = ArgumentParser()
     arguments.add_argument('--index-path', type=Path)
     args = arguments.parse_args()
 
-    hf_root = Path('datasets', 'open-llm-leaderboard')
-    for i in sys.stdin:
-        path = Path(i.strip())
-        name = hash_it(str(path), 12)
-
-        (*_, root, dot) = path.relative_to(hf_root).parents
-        assert dot == Path('.')
-
-        output = args.index_path.joinpath(root, name).with_suffix('.info')
+    for (g, v) in group(sys.stdin):
+        output = (args
+                  .index_path
+                  .joinpath(g.target, repr(g))
+                  .with_suffix('.info'))
         if output.exists():
             Logger.error('%s exists', output)
             continue
         output.parent.mkdir(parents=True, exist_ok=True)
-        output.write_text(i)
+        output.write_text('{}\n'.format('\n'.join(v)))
