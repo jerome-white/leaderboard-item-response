9a5bc696ba98133cd1f7eab47b49a6246622eee2
Jerome White <jerome@thewex.net>
2024-05-19 07:02:26 +0000

    Allow iterables to be written in addition to dictionaries

--- mylib/output_utils.py
+++ mylib/output_utils.py
@@ -1,15 +1,17 @@
 import csv
+import functools as ft
 from pathlib import Path
 from tempfile import NamedTemporaryFile
 from urllib.parse import urlunparse
+from collections.abc import Iterable
 
 import pandas as pd
 import awswrangler as wr
 
 class ChunkedDataWriter:
     def __init__(self, destination, chunk_size):
-        self.chunk_size = chunk_size
         self.destination = destination
+        self.chunk_size = chunk_size
         self.cache = []
 
     def __enter__(self):
@@ -19,13 +21,28 @@ class ChunkedDataWriter:
         if self.cache:
             self.flush()
 
-    def write(self, data: dict):
-        self.cache.append(data)
-        if len(self.cache) >= self.chunk_size:
+    def write_and_flush(self, data, push):
+        push(data)
+        cached = len(self.cache)
+        if cached >= self.chunk_size:
             self.flush()
             self.cache = []
+        else:
+            cached = 0
 
-        return not bool(self.cache)
+        return cached
+
+    @ft.singledispatchmethod
+    def write(self, data):
+        raise TypeError(type(data).__name__)
+
+    @write.register
+    def _(self, data: dict):
+        return self.write_and_flush(data, self.cache.append)
+
+    @write.register
+    def _(self, data: Iterable):
+        return self.write_and_flush(data, self.cache.extend)
 
     def flush(self):
         raise NotImplementedError()
@@ -46,17 +63,22 @@ class CSVFileWriter(ChunkedDataWriter):
             writer.writerows(self.cache)
 
 class SimpleStorageWriter(ChunkedDataWriter):
+    _conversions = {
+        'date': pd.to_datetime,
+        'value': pd.to_numeric,
+    }
+
     def __init__(self, destination, chunk_size):
         super().__init__(urlunparse(destination), chunk_size)
+        self.assign = {
+            x: lambda z: y(z[x]) for (x, y) in self._conversions.items()
+        }
 
     def flush(self):
         df = (pd
               .DataFrame
               .from_records(self.cache)
-              .astype({
-                  'date': 'datetime64[ns]',
-                  'value': float,
-              }))
+              .assign(**self.assign))
         wr.s3.to_parquet(
             df=df,
             path=self.destination,
