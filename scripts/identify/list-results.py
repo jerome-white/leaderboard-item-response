191af2fd5f4b69d115e31ea2726a79a99fd42574
Jerome White <jerome@thewex.net>
2025-01-13 07:52:20 +0000

    Updated method of navigating the evals

--- scripts/identify/list-results.py
+++ scripts/identify/list-results.py
@@ -11,67 +11,66 @@ from multiprocessing import Pool, Queue
 from huggingface_hub import HfFileSystem
 from huggingface_hub.utils import HfHubHTTPError
 
-from mylib import Logger, Backoff, DatasetPathHandler, hf_datetime
+from mylib import Logger, Backoff, DatasetPathHandler
 
 @dataclass
 class Result:
     path: Path
     dt: datetime
 
+    def __repr__(self):
+        (*prefix, _) = self.path.stem.split('_')
+        return str(Path(*prefix))
+
     def __lt__(self, other):
         return self.dt < other.dt
 
 class DatasetFileSystem:
-    def __init__(self, token, backoff):
-        self.fs = HfFileSystem(token=token)
+    def __init__(self, backoff):
         self.backoff = backoff
+        self.fs = HfFileSystem()
+        self.path = DatasetPathHandler()
 
-    def walk(self, target):
+    def ls(self, target):
+        target = self.path.to_string(target)
         for (i, j) in enumerate(self.backoff, 1):
             try:
                 yield from self.fs.ls(target)
                 break
-            except HfHubHTTPError as err:
+            except Exception as err:
                 Logger.error(
-                    '%s: request=%s message=%s (attempt=%d, backoff=%ds)',
+                    '%s: %s (attempt=%d, backoff=%ds)',
+                    type(err),
                     err,
-                    err.request_id,
-                    err.server_message,
                     i,
                     j,
                 )
             time.sleep(j)
 
-    def options(self, target):
-        for i in self.walk(target):
-            if i['type'] == 'directory':
-                path = Path(i['name'])
-                try:
-                    dt = hf_datetime(path.name)
-                except ValueError:
-                    continue
-                yield Result(path, dt)
-
-    def results(self, target):
-        for i in self.walk(target):
-            yield i['name']
+    def walk(self, target):
+        for i in self.ls(target):
+            name = i['name']
+            if self.fs.isdir(name):
+                for j in self.ls(name):
+                    path = Path(j['name'])
+                    if path.stem.startswith('samples_'):
+                        yield Result(path, j['last_commit'].date)
 
 def func(incoming, outgoing, args):
-    backoff = Backoff(args.backoff, 0.1)
-    fs = DatasetFileSystem(os.getenv('HF_BEARER_TOKEN'), backoff)
-    target = DatasetPathHandler()
+    fs = DatasetFileSystem(Backoff(args.backoff, 0.1))
+    results = {}
 
     while True:
         dataset = incoming.get()
         Logger.info(dataset)
 
-        results = []
-        try:
-            focus = min(fs.options(target.to_string(dataset)))
-            results.extend(fs.results(target.to_string(focus.path)))
-        except (ValueError, FileNotFoundError) as err:
-            Logger.warning(f'{dataset}: {err}')
-        outgoing.put(results)
+        results.clear()
+        for i in fs.walk(dataset):
+            key = repr(i)
+            if key not in results or results[key] > i:
+                results[key] = i
+
+        outgoing.put([ x.path for x in results.values() ])
 
 if __name__ == '__main__':
     arguments = ArgumentParser()
