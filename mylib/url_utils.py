b15906027f3b891721bda25b44151c77e9431611
Jerome White <jerome@thewex.net>
2025-01-13 07:47:59 +0000

    Support for strings

--- mylib/url_utils.py
+++ mylib/url_utils.py
@@ -1,3 +1,5 @@
+import functools as ft
+from pathlib import Path
 from urllib.parse import ParseResult, urlunparse
 
 class DatasetPathHandler:
@@ -12,13 +14,22 @@ class DatasetPathHandler:
             kwargs.setdefault(i, None)
         self.url = ParseResult(**kwargs)
 
+    @ft.singledispatchmethod
     def to_url(self, path):
+        raise TypeError(type(path))
+
+    @to_url.register
+    def _(self, path: Path):
         try:
             path = path.relative_to(self._netloc)
         except ValueError:
             pass
 
         return self.url._replace(path=str(path))
+
+    @to_url.register
+    def _(self, path: str):
+        return self.to_url(Path(path))
 
     def to_string(self, path):
         return urlunparse(self.to_url(path))
