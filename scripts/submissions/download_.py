916cda96b2513c0966ab9ddd1cbe1b7082ee509d
Jerome White <jerome@thewex.net>
2025-01-14 03:50:00 +0000

    Simplified submissions management

---
+++ scripts/submissions/download_.py
@@ -0,0 +1,148 @@
+import sys
+import csv
+import json
+from pathlib import Path
+from argparse import ArgumentParser
+from dataclasses import dataclass, fields, asdict, astuple
+from urllib.parse import ParseResult, urlunparse
+from multiprocessing import Pool, JoinableQueue
+
+import fsspec
+import requests
+import pandas as pd
+from huggingface_hub.utils import GatedRepoError, build_hf_headers
+
+from mylib import Logger, DatasetPathHandler
+
+#
+#
+#
+@dataclass
+class Result:
+    document: str
+    metric: str
+    score: float
+
+@dataclass(frozen=True)
+class GroupKey:
+    author: str
+    model: str
+
+#
+#
+#
+class DatasetAccessRequestor:
+    _url = {
+        'scheme': 'https',
+        'netloc': 'huggingface.co',
+    }
+    _endpoint = 'ask-access'
+
+    def __call__(self, path):
+        target = urlunparse(self.to_url(path))
+        headers = build_hf_headers()
+
+        response = requests.post(target, headers=headers)
+        response.raise_for_status()
+
+    def to_url(self, path):
+        body = path.parts[:3]
+        path = Path(*body, 'ask-access')
+
+        kwargs = dict(self._url, path=str(path))
+        for i in ParseResult._fields:
+            kwargs.setdefault(i, None)
+
+        return ParseResult(**kwargs)
+
+class HfFileReader:
+    def __init__(self):
+        self.ask = DatasetAccessRequestor()
+        self.path = DatasetPathHandler()
+
+    def __call__(self, target):
+        url = self.path.to_string(target)
+        attempted = False
+
+        while True:
+            try:
+                with fsspec.open(url) as fp:
+                    for line in fp:
+                        yield json.loads(line)
+                break
+            except GatedRepoError as err:
+                if attempted:
+                    raise PermissionError(target) from err
+                self.ask(target)
+                attempted = True
+
+class SubmissionReader:
+    _metrics = (
+        'acc',
+        'match',
+    )
+
+    def __init__(self):
+        self.reader = HfFileReader()
+
+    def __call__(self, df):
+        for i in df.itertuples(index=False):
+            view = i._asdict()
+            path = Path(view.pop('path'))
+            Logger.info(path)
+
+            try:
+                for r in self.results(path):
+                    record = dict(view)
+                    record.update(asdict(r))
+                    yield record
+            except PermissionError as err:
+                Logger.critical(err)
+
+    def results(self, path):
+        for line in self.reader(path):
+            document = line['doc_id']
+            for (metric, score) in line.items():
+                if any(metric.find(x) >= 0 for x in self._metrics):
+                    yield Result(document, metric, score)
+
+#
+#
+#
+def func(queue, args):
+    reader = SubmissionReader()
+
+    while True:
+        (key, group) = queue.get()
+        Logger.warning(key)
+
+        output = (args
+                  .output
+                  .joinpath(*astuple(key))
+                  .with_suffix('.csv.gz'))
+        output.parent.mkdir(parents=True, exist_ok=True)
+
+        df = pd.DataFrame.from_records(reader(group))
+        df.to_csv(output, index=False, compression='gzip')
+
+        queue.task_done()
+
+if __name__ == '__main__':
+    arguments = ArgumentParser()
+    arguments.add_argument('--output', type=Path)
+    arguments.add_argument('--workers', type=int)
+    args = arguments.parse_args()
+
+    queue = JoinableQueue()
+    initargs = (
+        queue,
+        args,
+    )
+
+    with Pool(args.workers, func, initargs):
+        by = [ x.name for x in fields(GroupKey) ]
+        df = pd.read_csv(sys.stdin, parse_dates=['date'])
+        for (i, g) in df.groupby(by, sort=False):
+            key = GroupKey(*i)
+            queue.put((key, g))
+        queue.task_done()
---
+++ scripts/submissions/gather_.py
@@ -0,0 +1,64 @@
+import sys
+import csv
+from typing import ClassVar
+from pathlib import Path
+from argparse import ArgumentParser
+from dataclasses import dataclass, asdict, fields
+
+import pandas as pd
+
+from mylib import Logger
+
+@dataclass
+class Sample:
+    author: str
+    model: str
+    name: str
+    category: str
+
+@dataclass
+class Submission:
+    path: Path
+    date: pd.Timestamp
+    _root: ClassVar[Path] = Path('samples', 'leaderboard')
+
+    def __post_init__(self):
+        self.path = Path(self.path)
+        self.date = pd.to_datetime(self.date)
+
+    def to_sample(self):
+        (*_, info, name) = self.path.parts
+        (author, model) = info.split('__')
+        path = Path(*name.split('_'))
+        (name, *category) = (path
+                             .relative_to(self._root)
+                             .parent
+                             .parts)
+        category = '_'.join(category)
+
+        return Sample(author, model, name, category)
+
+def records(fp):
+    reader = csv.DictReader(fp)
+    for row in reader:
+        submission = Submission(**row)
+        try:
+            sample = submission.to_sample()
+        except ValueError:
+            Logger.error(submission.path)
+            continue
+
+        rec = {}
+        for i in (submission, sample):
+            rec.update(asdict(i))
+
+        yield rec
+
+if __name__ == '__main__':
+    arguments = ArgumentParser()
+    arguments.add_argument('--output', type=Path)
+    arguments.add_argument('--workers', type=int)
+    args = arguments.parse_args()
+
+    df = pd.DataFrame.from_records(records(sys.stdin))
+    df.to_csv(sys.stdout, index=False)
