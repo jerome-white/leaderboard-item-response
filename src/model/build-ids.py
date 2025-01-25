113c12de8a53160454ca611826515be31ec6baca
Jerome White <jerome@thewex.net>
2025-01-25 05:40:31 +0000

    Operate from stdin

--- src/model/build-ids.py
+++ src/model/build-ids.py
@@ -1,10 +1,6 @@
 import sys
 import csv
-from pathlib import Path
-from argparse import ArgumentParser
-from tempfile import NamedTemporaryFile
 from dataclasses import dataclass, fields
-from multiprocessing import Pool
 
 from mylib import Logger
 
@@ -32,7 +28,7 @@ class AuthorModelHandler(VariableHandler):
         self.keys = tuple(x.name for x in fields(self.AuthorModel))
 
     def extract(self, data):
-        return self.AuthorModel(*map(data.get, keys))
+        return self.AuthorModel(*map(data.get, self.keys))
 
 class DocumentHandler(VariableHandler):
     def extract(self, data):
@@ -41,49 +37,23 @@ class DocumentHandler(VariableHandler):
 #
 #
 #
-class DocuScan:
-    def __init__(self):
-        self.handlers = {
-            'document_id': DocumentHandler(),
-            'author_model_id': AuthorModelHandler(),
-        }
+def scanf(fp):
+    handlers = {
+        'document_id': DocumentHandler(),
+        'author_model_id': AuthorModelHandler(),
+    }
 
-    def __call__(self, path):
-        for row in self.scanf(path):
-            for (k, handle) in self.handlers.items():
-                row[k] = handle(row)
-            yield row
-
-    def scanf(self, path):
-        for i in path.iterdir():
-            with i.open() as fp:
-                reader = csv.DictReader(fp)
-                assert not any(x in reader.fieldnames for x in self.handlers)
-                yield from reader
-
-def func(incoming, outgoing):
-    scanner = DocuScan()
-
-    while True:
-        path = incoming.get()
-        Logger.info(path)
-
-        writer = None
-        with NamedTemporaryFile(mode='w', dir=path.parent, delete=False) as fp:
-            for row in scanner(path):
-                if writer is None:
-                    writer = csv.DictWriter(fp, fieldnames=row)
-                    writer.writeheader()
-                writer.writerow(row)
-            outgoing.put(fp.name)
+    reader = csv.DictReader(fp)
+    assert not any(x in reader.fieldnames for x in handlers)
+    for row in reader:
+        for (k, handle) in handlers.items():
+            row[k] = handle(row)
+        yield row
 
 if __name__ == '__main__':
-    arguments = ArgumentParser()
-    arguments.add_argument('--source', type=Path)
-    arguments.add_argument('--workers', type=int)
-    args = arguments.parse_args()
-
-    with Pool(args.workers) as pool:
-        iterable = (Path(x.rstrip()) for x in sys.stdin)
-        for i in pool.imap_unordered(func, iterable):
-            print(i)
+    writer = None
+    for row in scanf(sys.stdin):
+        if writer is None:
+            writer = csv.DictWriter(sys.stdout, fieldnames=row)
+            writer.writeheader()
+        writer.writerow(row)
