2600f3884d27d154387cc807683fc926ac7a0fdf
Jerome White <jerome@thewex.net>
2025-01-25 12:47:50 +0530

    Ensure metric is present in the results

--- src/model/aggregate-data.py
+++ src/model/aggregate-data.py
@@ -2,19 +2,39 @@ import sys
 import csv
 from pathlib import Path
 from argparse import ArgumentParser
-from multiprocessing import Pool, Queue
+from multiprocessing import Pool
 
 import pandas as pd
 
 from mylib import Logger
 
-def func(incoming, outgoing, benchmark):
-    items = [
-        'author',
-        'model',
-        'document',
-        'score',
-    ]
+def func(args):
+    (path, metric) = args
+    Logger.info(path)
+
+    mkey = 'metric'
+    df = pd.read_csv(path, compression='gzip', memory_map=True)
+    if metric not in df[mkey]:
+        Logger.error(
+            '%s: metric \"%s\" not found (%s)',
+            path,
+            metric,
+            ', '.join(df[mkey].unique())
+        )
+        return
+
+    return (df
+            .groupby(mkey, sort=False)
+            .get_group(metric)
+            .filter(items=[
+                'author',
+                'model',
+                'document',
+                'score',
+            ])
+            .to_dict(orient='records'))
+
+def each(args):
     metric = {
         'arc': 'acc_norm',
         'bbh': 'acc_norm',
@@ -24,20 +44,10 @@ def func(incoming, outgoing, benchmark):
         'math': 'exact_match',
         'gsm8k': 'exact_match',
         'ifeval': 'prompt_level_strict_acc',
-    }[benchmark]
+    }[args.source.name]
 
-    while True:
-        path = incoming.get()
-        Logger.info(path)
-
-        records = (pd
-                   .read_csv(path, compression='gzip', memory_map=True)
-                   .groupby('metric', sort=False)
-                   .get_group(metric)
-                   .filter(items=items)
-                   .to_dict(orient='records'))
-
-        outgoing.put(records)
+    for p in args.source.rglob('*.csv.gz'):
+        yield (p, metric)
 
 if __name__ == '__main__':
     arguments = ArgumentParser()
@@ -45,24 +55,11 @@ if __name__ == '__main__':
     arguments.add_argument('--workers', type=int)
     args = arguments.parse_args()
 
-    incoming = Queue()
-    outgoing = Queue()
-    initargs = (
-        outgoing,
-        incoming,
-        args.source.name,
-    )
-
-    with Pool(args.workers, func, initargs):
-        jobs = 0
-        for i in args.source.rglob('*.csv.gz'):
-            outgoing.put(i)
-            jobs += 1
-
+    with Pool(args.workers) as pool:
         writer = None
-        for _ in range(jobs):
-            records = incoming.get()
-            if writer is None:
-                writer = csv.DictWriter(sys.stdout, fieldnames=records[0])
-                writer.writeheader()
-            writer.writerows(records)
+        for i in pool.imap_unordered(func, each(args)):
+            if i:
+                if writer is None:
+                    writer = csv.DictWriter(sys.stdout, fieldnames=i[0])
+                    writer.writeheader()
+                writer.writerows(i)
