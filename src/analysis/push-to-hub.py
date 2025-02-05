23576383244af85614684c48d5144981aaa5f3c3
Jerome White <jerome@thewex.net>
2025-02-05 13:55:17 +0000

    Another attempt to disable Hugging Face TQDM

--- src/analysis/push-to-hub.py
+++ src/analysis/push-to-hub.py
@@ -4,13 +4,13 @@ from argparse import ArgumentParser
 from tempfile import TemporaryDirectory
 
 import pandas as pd
-from datasets import Dataset
+from datasets import Dataset, disable_progress_bars
 
-def reader(fp, window):
-    with pd.read_csv(fp, iterator=True) as reader:
-        df = reader.get_chunk(window)
-        for i in df.itertuples(index=False):
-            yield i._asdict()
+def reader(fp, chunksize):
+    with pd.read_csv(fp, chunksize=chunksize) as reader:
+        for df in reader:
+            for i in df.itertuples(index=False):
+                yield i._asdict()
 
 if __name__ == '__main__':
     arguments = ArgumentParser()
@@ -19,13 +19,14 @@ if __name__ == '__main__':
     arguments.add_argument('--window', type=int, default=int(1e5))
     args = arguments.parse_args()
 
+    disable_progress_bars()
     with TemporaryDirectory() as cache_dir:
         dataset = Dataset.from_generator(
             reader,
             cache_dir=cache_dir,
             gen_kwargs={
                 'fp': sys.stdin,
-                'window': args.window,
+                'chunksize': args.window,
             },
             split=args.split,
         )
