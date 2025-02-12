d7245960278826b48ecc3c02cc12dc1aa49e3618
Jerome White <jerome@thewex.net>
2025-02-12 16:27:08 +0530

    Estimate values for an item response curve

---
+++ src/analysis/item-response-curve/estimate.py
@@ -0,0 +1,93 @@
+import sys
+import csv
+from pathlib import Path
+from argparse import ArgumentParser
+from dataclasses import dataclass
+from multiprocessing import Pool, Queue
+
+import numpy as np
+import pandas as pd
+
+from mylib import Logger
+
+@dataclass
+class ItemGroup:
+    source: str
+    df: pd.DataFrame
+
+    def __str__(self):
+        return self.source
+
+class ItemResponseCurve:
+    def __init__(self, theta):
+        self.theta = theta
+
+    def __call__(self, x):
+        return 1 / (1 + np.exp(-x['alpha'] * (self.theta - x['beta'])))
+
+def items(path):
+    parameter = 'parameter'
+    df = pd.read_csv(path, memory_map=True)
+    mask = df[parameter].isin(['alpha', 'beta'])
+
+    for (s, g) in df[mask].groupby('source', sort=False):
+        view = (g
+                .pivot(index=['chain', 'sample'],
+                       columns=parameter,
+                       values='value')
+                .assign(item=s))
+        yield ItemGroup(s, view)
+
+def func(incoming, outgoing, args):
+    abilities = np.linspace(
+        args.min_ability,
+        args.max_ability,
+        num=args.n_ability,
+    )
+
+    while True:
+        group = incoming.get()
+        Logger.info(group)
+
+        for a in abilities:
+            records = (group
+                       .df
+                       .assign(irc=irc(a))
+                       .filter(items=['item', 'irc'])
+                       .to_dict(orient='records'))
+            outgoing.put(records)
+        outgoing.put(None)
+
+if __name__ == '__main__':
+    arguments = ArgumentParser()
+    arguments.add_argument('--samples', type=Path)
+    arguments.add_argument('--min-ability', type=int)
+    arguments.add_argument('--max-ability', type=int)
+    arguments.add_argument('--n-ability', type=int, default=100)
+    arguments.add_argument('--workers', type=int)
+    args = arguments.parse_args()
+
+    incoming = Queue()
+    outgoing = Queue()
+    initargs = (
+        outgoing,
+        incoming,
+        args,
+    )
+
+    with Pool(args.workers, func, initargs):
+        jobs = 0
+        for i in items(args.samples):
+            outgoing.put(i)
+            jobs += 1
+
+        writer = None
+        while jobs:
+            rows = incoming.get()
+            if rows is None:
+                jobs -= 1
+            else:
+                if writer is None:
+                    writer = csv.DictWriter(sys.stdout, fieldnames=rows[0])
+                    writer.writeheader()
+                writer.writerows(rows)
