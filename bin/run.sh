31fe9d11a23d48749048e0fb2ffc5559f6478f1a
Jerome White <jerome@thewex.net>
2025-03-10 05:44:46 +0000

    Break process into numbered stages
    Allows users to specify which stage they want to run.

--- bin/run.sh
+++ bin/run.sh
@@ -3,75 +3,85 @@
 export GIT_ROOT=`git rev-parse --show-toplevel`
 source $GIT_ROOT/config.rc || exit 1
 
-_data=$SCRATCH/var/data
-_docs=$SCRATCH/var/documents
+_responses=$SCRATCH/var/responses
+_questions=$SCRATCH/var/questions
 _results=$SCRATCH/opt
+
+while getopts 's:h' option; do
+    case $option in
+        s) _step=$OPTARG ;;
+        h)
+            cat <<EOF
+Usage: $0
+ -s step
+    1: Hugging Face download
+    2: Stan preparation
+    3: Stan sampling
+    4: Hugging Face upload
+EOF
+            exit 0
+            ;;
+        *)
+            echo -e Unrecognized option \"$option\"
+            exit 1
+            ;;
+    esac
+done
 
 huggingface-cli login --token $HF_BEARER_TOKEN &> /dev/null || exit 1
 
-#
-# Hugging Face download
-#
+case $_step in
+    1) # Hugging Face download
+	src=$GIT_ROOT/src/data
+	python $src/list_.py \
+	    | python $src/gather_.py \
+	    | python $src/reduce_.py --corpus $_responses \
+	    | python $src/download_.py \
+		     --output $_responses \
+		     --question-bank $_questions
+	;;
+    2) # Stan preparation
+	src=$GIT_ROOT/src/model
+	tmp=`mktemp`
+	script=aggregate-data
 
-src=$GIT_ROOT/src/data
+	for i in $GIT_ROOT/src/experiments/*.py; do
+	    python $i --output $_results \
+		| while read; do
+		echo "[ `date` ] $REPLY" 1>&2
+		out=`dirname $REPLY`
 
-python $src/list_.py \
-    | python $src/gather_.py \
-    | python $src/reduce_.py --corpus $_data \
-    | python $src/download_.py \
-	     --output $_data \
-	     --question-bank $_docs
+		agg=$out/${script}.csv
+		python $src/${script}.py \
+		       --data-root $_responses \
+		       --question-bank $_questions \
+		       --experiment $REPLY > $agg
 
-#
-# Stan preparation
-#
-
-src=$GIT_ROOT/src/model
-tmp=`mktemp`
-script=aggregate-data
-
-for i in $GIT_ROOT/src/experiments/*.py; do
-    python $i --output $_results \
-	| while read; do
-	out=`dirname $REPLY`
-
-	agg=$out/${script}.csv
-	python $src/${script}.py \
-	       --data-root $_data \
-	       --question-bank $_docs \
-	       --experiment $REPLY > $agg
-
-	python $src/build-ids.py < $agg > $tmp
-	for j in stan variables; do
-	    cat <<EOF
+		python $src/build-ids.py < $agg > $tmp
+		for j in stan variables; do
+		    cat <<EOF
 python $src/to-${j}.py --data-file $tmp > $out/$j.json
 EOF
-	done | parallel --will-cite --line-buffer
+		done | parallel --will-cite --line-buffer
 
-	pigz --best $agg
-    done
-done
+		pigz --best $agg
+	    done
+	done
 
-rm $tmp
-
-#
-# Stan sampling
-#
-
-for d in $SCRATCH/opt/*; do
-    echo "[ START `date` ] $d" 1>&2
-    $GIT_ROOT/bin/sample.sh -d $d -w 500
-done
-
-#
-# Hugging Face upload
-#
-
-for i in $SCRATCH/opt/*; do
-    if [ -e $i/summary.csv ]; then
-	echo "[ `date` ] $i" 1>&2
-	split=`basename $i`
-	cat <<EOF
+	rm $tmp
+	;;
+    3) # Stan sampling
+	for d in $SCRATCH/opt/*; do
+	    echo "[ START `date` ] $d" 1>&2
+	    $GIT_ROOT/bin/sample.sh -d $d -w 500
+	done
+	;;
+    4) # Hugging Face upload
+	for i in $SCRATCH/opt/*; do
+	    if [ -e $i/summary.csv ]; then
+		echo "[ `date` ] $i" 1>&2
+		split=`basename $i`
+		cat <<EOF
 python $src/from-stan.py $sample \
        --stan-output $i/output \
        --parameters $i/variables.json \
@@ -79,8 +89,12 @@ python $src/from-stan.py $sample \
 	     --split $split \
 	     --target $HF_DATASETS_TARGET_
 EOF
-    fi
-done | parallel --will-cite --line-buffer
+	    fi
+	done | parallel --will-cite --line-buffer
+	;;
+    *)
+	;;
+esac
 
 #
 # Shutdown
