36ac14cef9b98589a0d234eee422df84dac75c63
Jerome White <jerome@thewex.net>
2026-09-24 20:38:10 +0530

    Make INFO the default log level

--- mylib/_logger.py
+++ mylib/_logger.py
@@ -7,7 +7,7 @@ import logging
 logging.basicConfig(
     format='[ %(asctime)s %(levelname)s %(filename)s ] %(message)s',
     datefmt='%H:%M:%S',
-    level=os.environ.get('PYTHONLOGLEVEL', 'WARNING').upper(),
+    level=os.environ.get('PYTHONLOGLEVEL', 'INFO').upper(),
 )
 logging.captureWarnings(True)
 Logger = logging.getLogger(__name__)
